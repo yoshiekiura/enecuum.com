b80cda845656f573d121731a4bc104718090793f
AwesomeNiko <my6JluH@gmail.com>
2018-06-22 05:48:11 +0300

    common updates

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,13 @@
-export default function ({store, redirect}) {
+export default function ({store, route, redirect}) {
   let isAuthed = store.dispatch('isAuth', {cookies: store.state.cookies});
   return isAuthed.then(res => {
-    if (res !== 'success') {
-      return redirect('/signin');
+      if (res !== 'success') {
+        return redirect('/auth/login');
+      }
+      let statusCode = store.state.kyc.code;
+      if (route.name !== 'backoffice' && statusCode === 423) {
+        return redirect('/backoffice');
+      }
     }
-  });
+  );
 }
