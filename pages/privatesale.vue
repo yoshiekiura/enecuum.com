d1def494d9a64888de7a7988aa54b49f83fde15c
AwesomeNiko <my6JluH@gmail.com>
2018-06-22 05:49:14 +0300

    add privatsale page && strings status codes

--- pages/privatesale.vue
+++ pages/privatesale.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container faq">
     <h1 class="text-center page-title">Private Sale FAQ</h1>
-    <el-row class="flex-center events faq-tabs">
+    <el-row class="flex-center events enq-tabs faq-tabs">
       <el-col :xs="22" :sm="16" :md="16" :lg="14" :xl="14">
         <el-collapse v-model="activeCollapse" accordion class="enq-collapse">
           <el-collapse-item v-for="(item, key) in faq" :key="key" :title="item.title" :name="item.title"
@@ -11,18 +11,6 @@
             </div>
           </el-collapse-item>
         </el-collapse>
-      </el-col>
-    </el-row>
-    <div class="divider"></div>
-    <el-row class="flex-center">
-      <el-col :xs="22" :sm="16" :md="16" :lg="14" :xl="14">
-        <p class="text-center"><b>This FAQ is very helpful, but what if I have more questions?​​</b></p>
-      </el-col>
-    </el-row>
-    <el-row class="flex-center">
-      <el-col :xs="22" :sm="16" :md="16" :lg="14" :xl="14">
-        <p class="text-center">If you have any more questions, please email our support at <br> <a
-          href="mailto:support@enecuum.com">support@enecuum.com</a></p>
       </el-col>
     </el-row>
   </section>
