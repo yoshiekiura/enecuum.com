d2b222081ec9584149a71a8f3286a58adfee788d
AwesomeNiko <my6JluH@gmail.com>
2018-06-27 02:14:36 +0300

    fix socket port

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io((process.env.dev ? 'http' : 'https') + '://enecuum.com:8081', {
+const socket = io((process.env.dev ? 'http' : 'https') + '://enecuum.com', {
   path: '/io',
   reconnectionAttempts: 30,
   reconnectionDelay: 3000
